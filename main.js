a458c5400bb52f8ee2d589325ad3526ad4496716
Ricardo Ruiz <rrc8596@gmail.com>
2023-05-19 22:09:06 -0600

    update

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ document.getElementById('searchButton').addEventListener('click', function () {
 });
 
 function getWeather(cityName) {
+
+    document.getElementById('weatherContainer').innerHTML = "";
     var apiKey = '4b362f15a49d41359b10deeaf50b0746';  // Replace with your actual OpenWeatherMap API Key
     var url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric&lang=es`;
 
@@ -13,22 +15,22 @@ function getWeather(cityName) {
             if (data.weather && data.weather.length > 0) {
                 var iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
                 document.getElementById('weatherContainer').innerHTML = `
-                    <h2>Clima en ${data.name}</h2>
-                    <img src="${iconUrl}" alt="Weather icon">
-                    <p>${data.weather[0].description}</p>
-                    <p>Temperatura: ${data.main.temp}°C</p>
-                    <p>Humedad: ${data.main.humidity}%</p>
-                `;
+                <h2>Clima en ${data.name}</h2>
+                <img src="${iconUrl}" alt="Weather icon">
+                <p>${data.weather[0].description}</p>
+                <p>Temperatura: ${data.main.temp}°C</p>
+                <p>Humedad: ${data.main.humidity}%</p>
+                <div id="map" class="map" style="width: 100%; height: 400px;"></div>
+            `;
                 displayMap(data.coord.lat, data.coord.lon);
             } else {
                 document.getElementById('weatherContainer').innerHTML = `
-                    <h2>No se pudo obtener el clima para ${cityName}</h2>
-                `;
+                <h2>No se pudo obtener el clima para ${cityName}</h2>
+            `;
             }
         })
         .catch(error => console.error('Error:', error));
 }
-
 
 function displayMap(lat, lon) {
     var map = new ol.Map({
@@ -40,7 +42,7 @@ function displayMap(lat, lon) {
         ],
         view: new ol.View({
             center: ol.proj.fromLonLat([lon, lat]),
-            zoom: 4
+            zoom: 12
         })
     });
 }
