a3db5f9f9f5db2ed41a3ae6a63d641e149adb221
Ricardo Ruiz <rrc8596@gmail.com>
2023-05-19 19:09:47 -0600

    update

---
+++ main.js
@@ -0,0 +1,27 @@
+document.getElementById('searchButton').addEventListener('click', function () {
+    var cityName = document.getElementById('searchInput').value;
+    getWeather(cityName);
+});
+
+function getWeather(cityName) {
+    var apiKey = '4b362f15a49d41359b10deeaf50b0746';  // Replace with your actual OpenWeatherMap API Key
+    var url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            if (data.weather && data.weather.length > 0) {
+                document.getElementById('weatherContainer').innerHTML = `
+                <h2>Weather in ${data.name}</h2>
+                <p>${data.weather[0].description}</p>
+                <p>Temperature: ${data.main.temp}°C</p>
+                <p>Humidity: ${data.main.humidity}%</p>
+            `;
+            } else {
+                document.getElementById('weatherContainer').innerHTML = `
+                <h2>Could not get weather for ${cityName}</h2>
+            `;
+            }
+        })
+        .catch(error => console.error('Error:', error));
+}
