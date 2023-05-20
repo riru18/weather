982ec3f17fb1a2e496379c0546e0e5b8b0ecbf9b
Ricardo Ruiz <rrc8596@gmail.com>
2023-05-19 21:40:55 -0600

    update

--- main.js
+++ main.js
@@ -4,24 +4,43 @@ document.getElementById('searchButton').addEventListener('click', function () {
 });
 
 function getWeather(cityName) {
-    var apiKey = '4b362f15a49d41359b10deeaf50b0746';
-    var url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+    var apiKey = '4b362f15a49d41359b10deeaf50b0746';  // Replace with your actual OpenWeatherMap API Key
+    var url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric&lang=es`;
 
     fetch(url)
         .then(response => response.json())
         .then(data => {
             if (data.weather && data.weather.length > 0) {
+                var iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
                 document.getElementById('weatherContainer').innerHTML = `
-                <h2>Weather in ${data.name}</h2>
-                <p>${data.weather[0].description}</p>
-                <p>Temperature: ${data.main.temp}°C</p>
-                <p>Humidity: ${data.main.humidity}%</p>
-            `;
+                    <h2>Clima en ${data.name}</h2>
+                    <img src="${iconUrl}" alt="Weather icon">
+                    <p>${data.weather[0].description}</p>
+                    <p>Temperatura: ${data.main.temp}°C</p>
+                    <p>Humedad: ${data.main.humidity}%</p>
+                `;
+                displayMap(data.coord.lat, data.coord.lon);
             } else {
                 document.getElementById('weatherContainer').innerHTML = `
-                <h2>Could not get weather for ${cityName}</h2>
-            `;
+                    <h2>No se pudo obtener el clima para ${cityName}</h2>
+                `;
             }
         })
         .catch(error => console.error('Error:', error));
+}
+
+
+function displayMap(lat, lon) {
+    var map = new ol.Map({
+        target: 'map',
+        layers: [
+            new ol.layer.Tile({
+                source: new ol.source.OSM()
+            })
+        ],
+        view: new ol.View({
+            center: ol.proj.fromLonLat([lon, lat]),
+            zoom: 4
+        })
+    });
 }
